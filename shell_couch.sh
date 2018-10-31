44b9cd6369a0e7ec1c14594bc36fe0ce4cd814e2
kananigit <kanani.joshua@gmail.com>
2018-10-30 22:04:57 -0500

    Edit the update_doc method and write more requirements

--- shell_couch.sh
+++ shell_couch.sh
@@ -98,19 +98,30 @@ function view_doc(){
 }
 
 #Update a document in couchdb
-#This function still needs work. Need to find a way to get the _revision value. Could try using awk, and sed in bash
 function update_doc(){
   local id="$1"
   local db="$2"
   
-  
+  #make a json file of the wordpress post to be updated
+  wp post get "$id" --format=json > temp_file.json --allow-root
+
   #get the _revision value .I love the jq comandline JSON  processor be sure to install it and use it to get the rev value
-  local rev_id="$(curl -X GET http://127.0.0.1:5984/"$db"/"$id" | jq '._rev')"
+  local rev_id="$(curl -X GET http://127.0.0.1:5984/"$db"/"$id" | jq -r '._rev')"
+  #echo "$rev_id"
+  
+  #using jq add the _rev value of the couchdb document to be updated to the wp json post and output to a new file 
+  cat temp_file.json | jq --arg revision_id $rev_id '. + {_rev: $revision_id}' > temp_file_with_rev.json
+  
+  #Update the couchdb document with the modified post from wordpress
+  curl -X PUT -H "Content-type: application/json" http://127.0.0.1:5984/"$db"/"$id" -d@temp_file_with_rev.json
+
+  #You can then delete the temp files
+  rm temp_file.json temp_file_with_rev.json
+  
+
   #When Updating make sure you include all the fields that were originally there and your updated copy then update
   #Otherwise the new document will only have your one updated field
   #curl -X PUT http://127.0.0.1:5984/database_name/document_id/ -d '{ "field" : "value", "_rev" : '$rev_id' }'
-
-  curl -X PUT http://127.0.0.1:5984/"$db"/"$id" -d ' { " age " : " 24 " , " _rev " : " 1-1c2fae390fa5475d9b809301bbf3f25e " } '
   #In return JSON contains the success message, the ID of the document being updated, and the new revision information. 
   #If you want to update the new version of the document, you have to quote this latest revision number.
 }
@@ -121,11 +132,17 @@ function update_doc(){
 
 function attach_doc(){
 
+  local id="$1"
+  local db="$2"
+  local rev_id="$(curl -X GET http://127.0.0.1:5984/"$db"/"$id" | jq -r '._rev')"  #use jq library to process json
+  #echo "$rev_id"
+
   #curl -vX PUT http://127.0.0.1:5984/db_name/doc_id/filename?rev=doc_rev_id --data-binary @filename -H "Content-Type:type of the content"
   #--data-binary@ - This option tells cURL to read a file’s contents into the HTTP request body.
 
-  #curl -vX PUT http://127.0.0.1:5984/my_database/001/boy.jpg?rev=1-967a00dff5e02add41819138abb3284d --data-binary @boy.jpg -H "ContentType:#image/jpg"
+  curl -vX PUT http://127.0.0.1:5984/"$db"/"$id"/en-gcf2015.png?rev="$rev_id" --data-binary @en-gcf2015.png -H "ContentType:#image/png"
 }
+
 
 
 
@@ -153,9 +170,30 @@ echo  "----------------/n"
 #Error checking and handling in the functions
 #Finish working on Update function
 # Use python to make Json files from bash commands then use curl to put the different info got into couchdb from a different bash script
-#check date modified in couchdb document and match with the one in wordpress post if different then update the couchdb document
 #Writing views to couchdb using curl
 #Examples on this site https://www.lullabot.com/articles/a-recipe-for-creating-couchdb-views
+#Writing a view to get the date modified fields
+#Working on writing wp posts from couchdb documents
+#Attaching WP sql exported databases to couchdb. Have each document with backup date as the id, then the attachment
+
+#Checking for new wordpress posts and uploading to couchdb
+#  -Always have a copy of post IDS
+#  -in a function read the text file and save the IDS in an array
+#  -make another array of current wordpress post IDS
+#  -compare the arrays and make another array with the difference in the 2 arrays. 
+#  -then using the ids in the new array get their wordpress posts and convert them to couchdb database
+#  -get all the wordpress current ids and rewrite the original IDS text file
+
+#Updating a couchdb document with an updated wordpress post  --DONE
+# -check date modified in couchdb document and match with the one in wordpress post if different then update the couchdb document
+#  -in a loop using the id as the variable, get date_modified from couchdb document and from wordpress post and compare
+#  -If the date modified in the wordpress post is greater than the couchdb document then update the couch document with the new wp post
+# -couchdb: curl -X GET http://127.0.0.1:5984/"$db"/"$id" | jq -r '.post_modified'
+# -wpcli: wp post get $id --field=post_modified --allow-root
+# -get current date : DATE=`date '+%Y-%m-%d %H:%M:%S'`
+# -convert date and time to number : todate=$(date -d "$DATE" +"%Y%m%d%H%M%S")   or for just date todate=$(date -d "$DATE" +"%Y%m%d")
+# -
+
 
 
 
